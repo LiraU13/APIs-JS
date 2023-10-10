f687b0abb692ed48d0e2b7ae22f02da35dda269d
Ulises Lira <moredialiraulises13@gmail.com>
2023-10-10 12:40:24 -0600

    Rick and Morty Parte 2

--- RickandMorty/app.js
+++ RickandMorty/app.js
@@ -1,18 +1,35 @@
 const URL ='https://rickandmortyapi.com/api';
 const containerChar = document.querySelector('.characters')
+const pagination = document.querySelector('.pagination');
 
-fetch(` ${ URL }/character `)
+function createPag() {
+    let buttons = ``;
+    for (let i = 1; i <= 42; i++) {
+        buttons += `
+        <li class="page-item">
+            <a class="page-link" href="#" data-id="${ i }">${ i }</a>
+        </li>`
+    }
+    pagination.innerHTML = buttons;
+}
+
+createPag();
+
+function getCharacters(page=1) {
+    fetch(` ${ URL }/character/?page=${ page } `)
     .then(response => response.json())
     .then(data => {
         console.log(data.info);
         const characters = data.results;
         showCharacteres(characters);
-    })
+    })    
+}
 
+getCharacters();
 
 function createCard(character){
     const card = document.createElement('div');
-    card.classList.add('card');
+    card.classList.add('card', 'mt-3', 'bg-secondary-subtle');
     card.style.width = '18rem';
     const htmlCard = `  <img src="${character.image }" class="card-img-top" alt="...">
     <div class="card-body">
@@ -20,8 +37,8 @@ function createCard(character){
       <p class="card-text">${character.status}</p>
       <p class="card-text">${character.origin.name}</p>
       <a href="#" 
-            class="btn btn-primary" 
-            data-id= ${character.id}>Go somewhere</a>
+            class="btn btn-success" 
+            data-id= ${character.id}>Ver más</a>
     </div>`
     card.innerHTML = htmlCard;
     return card;
@@ -33,3 +50,13 @@ function showCharacteres(characters) {
     })
     
 }
+
+function getButton(e){
+    e.preventDefault();
+    if(e.target.classList.contains('page-link')){
+        id = e.target.getAttribute('data-id');
+        getCharacters(id);
+    }
+}
+
+pagination.addEventListener('click', getButton);
